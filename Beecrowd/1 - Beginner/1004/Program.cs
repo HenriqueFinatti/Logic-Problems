d824de67bfef2854c4eb6aad9b50ca90d7e01c17
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-19 17:09:26 -0300

    1004 - Beecrowd

---
+++ Beecrowd/1 - Beginner/1004/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number1 = int.Parse(Console.ReadLine());
+        int number2 = int.Parse(Console.ReadLine());
+        int prod = number1 * number2;
+
+        Console.WriteLine("PROD = " + prod);
+    }
+}
