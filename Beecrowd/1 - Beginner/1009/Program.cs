5c0b0242da5c889aabebdfd3579641e1524c0717
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-19 19:24:55 -0300

    Salario Com Bonus

---
+++ Beecrowd/1 - Beginner/1009/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        double salary = double.Parse(Console.ReadLine());
+        double salesBonus = double.Parse(Console.ReadLine()) * 0.15;
+        double finalSalary = salary + salesBonus;
+
+        Console.WriteLine($"TOTAL = R$ {finalSalary:F2}");
+
+    }
+}
