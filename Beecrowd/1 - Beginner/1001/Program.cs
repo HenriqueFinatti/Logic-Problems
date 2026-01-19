6b4bf97537425820b0814924832d4b024e9a53c2
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-19 16:37:40 -0300

    1001 - Beecrowd

---
+++ Beecrowd/1 - Beginner/1001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number1 = int.Parse(Console.ReadLine());
+        int number2 = int.Parse(Console.ReadLine());
+        int sum = number1 + number2;
+
+        Console.WriteLine("X = " + sum);
+    }
+}
