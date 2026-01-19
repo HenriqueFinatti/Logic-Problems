1907934f4a187a34afc7245a57c93ebd71e4b632
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-19 16:47:07 -0300

    1002 - Beecrowd

---
+++ Beecrowd/1 - Beginner/1002/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal n = 3.14159m;
+        decimal r = Decimal.Parse(Console.ReadLine());
+        decimal area = n * (r*r);
+
+        Console.WriteLine($"A={area:F4}");
+    }
+}
