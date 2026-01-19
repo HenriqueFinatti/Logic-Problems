34da636483c04ab418266c5e930e18935a10fe21
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-19 17:11:42 -0300

    1005 - Beecrowd

---
+++ Beecrowd/1 - Beginner/1005/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double number1 = Convert.ToDouble(Console.ReadLine());
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        double avg = (number1 * 3.5 + number2 * 7.5) / 11;
+
+        Console.WriteLine($"MEDIA = {avg:F5}");
+    }
+}
