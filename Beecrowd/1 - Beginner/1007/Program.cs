b611e6e395001d4163866054800c7fadd6d5e481
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-19 19:05:24 -0300

    Diferença

---
+++ Beecrowd/1 - Beginner/1007/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A, B, C, D, diff;
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        C = int.Parse(Console.ReadLine());
+        D = int.Parse(Console.ReadLine());
+
+        diff = A * B - C * D;
+        Console.WriteLine("DIFERENCA = " + diff);
+    }
+}
