456d823404738f51a43e8e6d4d2ee0ab6589244d
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-19 19:17:17 -0300

    Salario

---
+++ Beecrowd/1 - Beginner/1008/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int employeeId = int.Parse(Console.ReadLine());
+        int hoursWorked = int.Parse(Console.ReadLine());
+
+        double hourValue = double.Parse(Console.ReadLine());
+        double salary = hoursWorked * hourValue;
+        Console.WriteLine("NUMBER = " + employeeId);
+        Console.WriteLine($"SALARY = U$ {salary:F2}");
+    }
+}
