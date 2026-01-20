0e7d15e28c2d1dc6a0d9098bd687ef5bdaf8a3b3
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-19 21:35:50 -0300

    Calculo Simples

---
+++ Beecrowd/1 - Beginner/1010/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] row1 = Console.ReadLine().Split(' ');
+        string[] row2 = Console.ReadLine().Split(' ');
+
+        int qtd1 = int.Parse(row1[1]);
+        int qtd2 = int.Parse(row2[1]);
+
+        double value1 = double.Parse(row1[2]) * qtd1;
+        double value2 = double.Parse(row2[2]) * qtd2;
+
+        Console.WriteLine($"VALOR A PAGAR: R$ {(value1 + value2):F2}");
+    }
+}
