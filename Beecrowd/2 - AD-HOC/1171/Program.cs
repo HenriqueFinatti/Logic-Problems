752af71e6b15e9eff9d2ff48912111cc8764374b
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-20 22:18:29 -0300

    Frequencia de numeros

---
+++ Beecrowd/2 - AD-HOC/1171/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int cases = int.Parse(Console.ReadLine());
+        SortedDictionary<int, int> numbersList = new SortedDictionary<int, int>();
+
+        while (cases > 0)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (numbersList.ContainsKey(number))
+            {
+                numbersList[number]++;
+            }
+            else
+            {
+                numbersList.Add(number, 1);
+            }
+            cases--;
+        }
+
+        foreach (int item in numbersList.Keys)
+        {
+            Console.WriteLine($"{item} aparece {numbersList[item]} vez(es)");
+        }
+    }
+}
