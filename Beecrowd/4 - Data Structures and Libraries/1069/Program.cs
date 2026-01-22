3eed8198e33f7d63e9a1e5553dd37f4d86bfe7f0
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-22 17:28:58 -0300

    Diamantes na areia

---
+++ Beecrowd/4 - Data Structures and Libraries/1069/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int casos = int.Parse(Console.ReadLine());
+
+        while (casos > 0)
+        {
+            string diamantesNaAreia = Console.ReadLine();
+            Stack<char> diamantesAbertos = new Stack<char>();
+            int diamantes = 0;
+            foreach (char item in diamantesNaAreia)
+            {
+                if (item == '<')
+                {
+                    diamantesAbertos.Push('<');
+                    continue;
+                }
+
+                if (item == '>' && diamantesAbertos.Count > 0)
+                {
+                    diamantes++;
+                    diamantesAbertos.Pop();
+                    continue;
+                }
+            }
+            Console.WriteLine(diamantes);
+
+            casos--;
+        }
+    }
+}
