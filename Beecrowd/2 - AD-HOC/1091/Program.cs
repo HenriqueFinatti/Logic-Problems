4f4b9a5e28563a3fbf5b740d28cc2b1dcd443960
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-27 20:59:34 -0300

    Divisao da Nlogonia

---
+++ Beecrowd/2 - AD-HOC/1091/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Inicio:
+        int casos = int.Parse(Console.ReadLine());
+
+        if (casos != 0)
+        {
+            int[] divisor = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int divisorX, divisorY;
+
+            divisorX = divisor[0];
+            divisorY = divisor[1];
+
+            for (int i = 0; i < casos; i++)
+            {
+                int[] casa = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                int casaX, casaY;
+
+                casaX = casa[0];
+                casaY = casa[1];
+
+                if (casaX == divisorX || casaY == divisorY)
+                {
+                    Console.WriteLine("divisa");
+                    continue;
+                }
+
+                if (casaY > divisorY)
+                {
+                    if (casaX > divisorX)
+                    {
+                        Console.WriteLine("NE");
+                        continue;
+                    }
+                    Console.WriteLine("NO");
+                    continue;
+                }
+
+                if (casaX > divisorX)
+                {
+                    Console.WriteLine("SE");
+                    continue;
+                }
+                Console.WriteLine("SO");
+            }
+            goto Inicio;
+        }
+    }
+}
