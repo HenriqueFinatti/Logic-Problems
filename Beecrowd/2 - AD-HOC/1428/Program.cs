bdf11ff50e4f0d4ce29eb528dbc37fafca1963cc
Henrique Finatti <finatti.henrique@gmail.com>
2026-01-27 21:07:09 -0300

    Procurando Nessy

---
+++ Beecrowd/2 - AD-HOC/1428/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int casos = int.Parse(Console.ReadLine());
+
+        while (casos > 0)
+        {
+            int[] entrada = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            double n = entrada[0];
+            double m = entrada[1];
+
+            int sensoresColunas = (int)Math.Floor(n/3);
+            int sensoresLinhas = (int)Math.Floor(m/3);
+
+            Console.WriteLine(sensoresColunas * sensoresLinhas);
+            casos--;
+        }
+    }
+}
